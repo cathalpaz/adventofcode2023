20109d6e8c3f39a25755d516ad334335119d8aff
Cathal <cathalpaz@gmail.com>
2023-12-03 18:04:33 -0500

    day1

--- day1/1.py
+++ day1/1.py
@@ -1,29 +1,9 @@
-def read_input():
-    with open('input.txt', 'r') as f:
-        lines = f.read()
-        lines = lines.split('\n')[:-1]
-        return lines
-
-
-##### HERE #####
-def program(lines):
+with open("input.txt") as f:
     res = 0
-    for line in lines:
+    for line in f.read().splitlines():
         digits = []
         for char in line:
             if char.isdigit():
                 digits.append(char)
-        total = int(digits[0] + digits[-1])
-        res += total
-
-    return res
-
-
-
-def main():
-    lines = read_input()
-    solution = program(lines)
-    print(solution)
-
-if __name__ == "__main__":
-    main()
+        res += int(digits[0] + digits[-1])
+    print(res)
