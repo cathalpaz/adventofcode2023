cefa62155bb6e3e3144c7c41c682c8d37ea4c60e
Cathal <cathalpaz@gmail.com>
2023-12-04 10:33:58 -0500

    d4p1

---
+++ day4/1.py
@@ -0,0 +1,23 @@
+with open('input.txt') as f:
+    res = 0
+    for line in f.read().splitlines():
+        count = 0
+        l = line.split(': ')[1:]
+        winning, nums = l[0].split(' | ')
+        winning = winning.split(' ')
+        winners = set()
+        for n in winning:
+            if n:
+                winners.add(n)
+
+        nums = nums.split(' ')
+        for n in nums:
+            if n and n in winners:
+                if not count:
+                    count += 1
+                else:
+                    count *= 2
+
+        res += count
+
+    print(res)
